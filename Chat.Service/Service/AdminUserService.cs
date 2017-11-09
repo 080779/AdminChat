ce124309bfd2ce58e8b20e4c2298fc5e575c8cee
080779 <exceed295@qq.com>
2017-11-09 10:26:07 +0800

    1553

---
+++ Chat.Service/Service/AdminUserService.cs
@@ -0,0 +1,68 @@
+﻿using Chat.IService.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chat.DTO.DTO;
+
+namespace Chat.Service.Service
+{
+    public class AdminUserService : IAdminUserService
+    {
+        public long AddAdminUser(string name, string phoneNum, string password, string email, long? cityId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CheckLogin(string phoneNum, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public AdminUserDTO[] GetAll()
+        {
+            throw new NotImplementedException();
+        }
+               
+        public AdminUserDTO GetById(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public AdminUserDTO GetByPhoneNum(string phoneNum)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool HasPermission(long adminUserId, string permissionName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool MarkDeleted(long adminUserId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RecordLoginError(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ResetLoginError(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateAdminUser(long id, string name, string email, long? cityId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdatePassword(long id, string Password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Chat.Service/Service/CityService.cs
@@ -0,0 +1,28 @@
+﻿using Chat.IService.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chat.DTO.DTO;
+
+namespace Chat.Service.Service
+{
+    public class CityService : ICityService
+    {
+        public long AddNew(string cityName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CityDTO[] GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public CityDTO GetById(long id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
